5594d44eacd88ce07d0c29f90b2bd99b89843ba2
Minh <theonething2001@gmail.com>
2022-06-13 20:39:38 +0700

    push github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { gapi } from 'gapi-script'
+import Login from './components/Login';
+import Home from './container/Home';
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    gapi.load('client:auth2', ()=>{
+      gapi.auth2.init({
+        client_id: process.env.REACT_APP_GOOGLE_API_TOKEN
+        })
+    })
+    if (!User) navigate('/login');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
